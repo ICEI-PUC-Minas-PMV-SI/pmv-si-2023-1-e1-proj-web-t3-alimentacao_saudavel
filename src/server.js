a48471b6b17dfdc73c532c7abc77ac63581fd613
ItaloFVN <ifvnasc@gmail.com>
2023-06-13 10:40:05 -0300

    correção da rota do database utilizado

--- src/server.js
+++ src/server.js
@@ -1,19 +1,14 @@
 // JSON Server module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db/db.json");
+const router = jsonServer.router("database/db.json");
 
 // Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 // Add this before server.use(router)
-server.use(
- // Add custom route here if needed
- jsonServer.rewriter({
-  "/api/*": "/$1",
- })
-);
+
 server.use(router);
 // Listen to port
 server.listen(3000, () => {
