af973b2208ec8bbe6a52b58b9a0f498ebca9baf5
ItaloFVN <ifvnasc@gmail.com>
2023-06-13 11:58:27 -0300

    correção para middlewares padrões

--- src/api/server.js
+++ src/api/server.js
@@ -5,9 +5,7 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-}))
+
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
