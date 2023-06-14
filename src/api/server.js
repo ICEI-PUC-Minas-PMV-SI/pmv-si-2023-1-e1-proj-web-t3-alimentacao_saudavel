560cb17e850d7c30fa2fae8ff8cd672073d5469e
ItaloFVN <ifvnasc@gmail.com>
2023-06-14 20:21:12 -0300

    Correção db json

--- src/api/server.js
+++ src/api/server.js
@@ -1,7 +1,7 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('database/db.json')
+const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
