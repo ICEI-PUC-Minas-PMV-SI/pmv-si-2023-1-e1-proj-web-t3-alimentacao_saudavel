5ad8c7f1635d5a1cc6e07fc0435c7d4b2544f557
ItaloFVN <ifvnasc@gmail.com>
2023-06-15 12:39:39 -0300

    retirada de rotas desnecessárias

--- api/server.js
+++ api/server.js
@@ -6,10 +6,7 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
+
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
