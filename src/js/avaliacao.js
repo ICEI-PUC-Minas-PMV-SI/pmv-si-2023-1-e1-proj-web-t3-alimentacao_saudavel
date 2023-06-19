eac359a925a68817b8e03fb8a69ac7bc700f7caa
Jdutra <dutrajully@icloud.com>
2023-06-19 18:31:05 -0400

    atualização da atualização da page

--- src/js/avaliacao.js
+++ src/js/avaliacao.js
@@ -57,73 +57,83 @@ async function salvarAvaliacaoNoServidor(avaliacao) {
 }
 
 // Função para exibir as avaliações no histórico da receita
-async function exibirAvaliacoes(receitaId) {
+function exibirAvaliacoes(receitaId) {
   const listaAvaliacoes = document.getElementById("listaAvaliacoes");
   listaAvaliacoes.innerHTML = ""; // Limpar a lista antes de exibir as avaliações
 
-  const avaliacoes = await obterAvaliacaoPorReceita(receitaId);
+  obterAvaliacaoPorReceita(receitaId)
+    .then((avaliacoes) => {
+      if (avaliacoes.length > 0) {
+        for (const avaliacao of avaliacoes) {
+          const itemAvaliacao = document.createElement("li");
+          const usuario = avaliacao.idUsuario === obterUsuarioLogadoId() ? "Você" : `Usuário ${avaliacao.idUsuario}`;
+          itemAvaliacao.textContent = `${usuario} - Avaliação: ${avaliacao.nota} estrela(s) - Comentário: ${avaliacao.avaliacaoReceita}`;
 
-  if (avaliacoes.length > 0) {
-    for (const avaliacao of avaliacoes) {
-      const itemAvaliacao = document.createElement("li");
-      const usuario = avaliacao.idUsuario === obterUsuarioLogadoId() ? "Você" : `Usuário ${avaliacao.idUsuario}`;
-      itemAvaliacao.textContent = `${usuario} - Avaliação: ${avaliacao.nota} estrela(s) - Comentário: ${avaliacao.avaliacaoReceita}`;
-
-      listaAvaliacoes.appendChild(itemAvaliacao);
-    }
-  }
+          listaAvaliacoes.appendChild(itemAvaliacao);
+        }
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
 // Função para calcular a média das avaliações
-async function calcularMediaAvaliacoes(receitaId) {
-  const avaliacoes = await obterAvaliacaoPorReceita(receitaId);
-  if (avaliacoes.length > 0) {
-    const totalAvaliacoes = avaliacoes.length;
-    let somaAvaliacoes = 0;
-    for (const avaliacao of avaliacoes) {
-      somaAvaliacoes += avaliacao.nota;
-    }
-    const mediaAvaliacoes = somaAvaliacoes / totalAvaliacoes;
+function calcularMediaAvaliacoes(receitaId) {
+  obterAvaliacaoPorReceita(receitaId)
+    .then((avaliacoes) => {
+      if (avaliacoes.length > 0) {
+        const totalAvaliacoes = avaliacoes.length;
+        let somaAvaliacoes = 0;
+        for (const avaliacao of avaliacoes) {
+          somaAvaliacoes += avaliacao.nota;
+        }
+        const mediaAvaliacoes = somaAvaliacoes / totalAvaliacoes;
 
-    const elementoMedia = document.getElementById("mediaAvaliacoes");
-    elementoMedia.textContent = `Média das avaliações: ${mediaAvaliacoes.toFixed(2)} estrela(s)`;
+        const elementoMedia = document.getElementById("mediaAvaliacoes");
+        elementoMedia.textContent = `Média das avaliações: ${mediaAvaliacoes.toFixed(2)} estrela(s)`;
 
-    const ratingElement = document.getElementById("ratingStars");
-    ratingElement.innerHTML = ""; // Limpar o conteúdo existente
+        const ratingElement = document.getElementById("ratingStars");
+        ratingElement.innerHTML = ""; // Limpar o conteúdo existente
 
-    // Gerar as estrelas preenchidas com base na média
-    for (let i = 1; i <= 5; i++) {
-      const starElement = document.createElement("i");
-      if (i <= mediaAvaliacoes) {
-        starElement.className = "bi bi-star-fill";
+        // Gerar as estrelas preenchidas com base na média
+        for (let i = 1; i <= 5; i++) {
+          const starElement = document.createElement("i");
+          if (i <= mediaAvaliacoes) {
+            starElement.className = "bi bi-star-fill";
+          } else {
+            starElement.className = "bi bi-star";
+          }
+          ratingElement.appendChild(starElement);
+        }
       } else {
-        starElement.className = "bi bi-star";
-      }
-      ratingElement.appendChild(starElement);
-    }
-  } else {
-    const elementoMedia = document.getElementById("mediaAvaliacoes");
-    elementoMedia.textContent = "Ainda não há avaliações";
+        const elementoMedia = document.getElementById("mediaAvaliacoes");
+        elementoMedia.textContent = "Ainda não há avaliações";
 
-    const ratingElement = document.getElementById("ratingStars");
-    ratingElement.innerHTML = ""; // Limpar o conteúdo existente
-  }
+        const ratingElement = document.getElementById("ratingStars");
+        ratingElement.innerHTML = ""; // Limpar o conteúdo existente
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
-// Evento de submissão do formulário de avaliação
-document.getElementById("avaliacaoForm").addEventListener("submit", function(event) {
+// Evento de clique no botão de enviar avaliação
+document.querySelector(".submit-button").addEventListener("click", function(event) {
   event.preventDefault();
 
   const avaliacao = document.querySelector("input[name='avaliacao']:checked").value;
   const comentario = document.getElementById("comentarios").value;
 
-  adicionarAvaliacao(avaliacao, comentario);
-
-  document.querySelector("input[name='avaliacao']:checked").checked = false;
-  document.getElementById("comentarios").value = "";
-
-  const receitaId = document.querySelector("main").dataset.receitaId; // Obter o ID da receita
-  exibirAvaliacoes(receitaId); // Chamar a função de exibição após adicionar uma avaliação
+  adicionarAvaliacao(avaliacao, comentario)
+    .then(() => {
+      document.querySelector("input[name='avaliacao']:checked").checked = false;
+      document.getElementById("comentarios").value = "";
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 });
 
 // Exibir avaliações iniciais da receita (se houver)
