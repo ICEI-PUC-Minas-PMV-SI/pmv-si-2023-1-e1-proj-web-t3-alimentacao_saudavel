ffa21662e6b9942db8c366eb962aa26351a1c330
Mariana Wandermurem <wandermurenmariana@gmail.com>
2023-06-20 11:38:41 -0300

    subida do arquivo js do calculo imc

---
+++ src/js/calculo_imc.js
@@ -0,0 +1,63 @@
+// PARA GUARDAR OS DADOS COLOCADOS NO FORMULARIO
+
+var dadosIMC = JSON.stringify({
+    sexo : parseFloat(document.getElementById('inputWeight').value),
+    peso : parseFloat(document.getElementById('inputWeight').value),
+    altura : parseFloat(document.getElementById('inputHeight').value)
+
+});
+
+// =======================================================================================
+
+
+// PARA EMITIR O RESULTADO DO IMC
+var IMC = peso / (altura * altura);
+document.getElementById("show-imc").addEventListener("click", function(){
+           if(IMC < 20)
+    		{
+    			alert('Você esta abaixo do peso!');
+    		} 
+    		else if(IMC >20 && IMC <= 25)
+    		{
+    			alert("Peso Ideal");
+    		}
+    		else if(IMC >25 && IMC <= 30)
+    		{
+    			alert("Sobrepeso");
+    		}
+    		else if(IMC >30 && IMC <= 35)
+    		{
+    			alert("Obesidade Moderada");
+    		}
+    		else if(IMC >35 && IMC <= 40)
+    		{
+    			alert("Obesidade Severa");
+    		}
+    		else if(IMC >40 && IMC <= 50)
+    		{
+    			alert("Obesidade Morbida");
+    		}
+    		else
+    		{
+    			alert('Gordo');
+    		}
+});
+// =======================================================================================
+
+// PARA CONCLUIR O CALCULO IMC DE UM USUÁRIO CADASTRADO
+var resultcad = (`${i} X ${nun} = ${i*nun}`);
+
+function dadosIMC (){
+    if (dadosUsuario == dadosUsuario){
+        alert('Olá ' + nome + ', é um prazer te ver!');
+       resultcad = dadosUsuario[0].peso / (dadosUsuario[0].altura*dadosUsuario[0].altura);
+    }
+    else{
+        alert('Faça seu cadastro se não ainda não tem');
+    }
+}
+
+
+// =======================================================================================
+
+
