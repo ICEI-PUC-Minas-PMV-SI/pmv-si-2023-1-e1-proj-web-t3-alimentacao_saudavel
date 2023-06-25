9e7f01329de09bcc98edc78a98beb39204762182
Mariana Wandermurem <wandermurenmariana@gmail.com>
2023-06-25 14:29:48 -0300

    criação do arquivo para o consumo de agua

---
+++ src/js/calculo_agua.js
@@ -0,0 +1,11 @@
+System.out.println("Informar o consumo");
+
+document.getElementById ('meta')({
+    peso : parseFloat(document.getElementById('inputWeight').value),
+});
+
+metadeagua = peso * 35;
+while ( consumo < metadeagua){
+   consumo : document.getElementById('inputHeight').value,
+   Console.WriteLine($metadeagua);
+}
