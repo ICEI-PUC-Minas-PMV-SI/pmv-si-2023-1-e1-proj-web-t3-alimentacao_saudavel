850623df829b7d84b7dca93fd4002836d219f1f2
Mariana Wandermurem <wandermurenmariana@gmail.com>
2023-06-29 20:07:03 -0300

    UPDATE REGISTROS AGUA IMC

---
+++ src/js/calculo_agua.js
@@ -0,0 +1,54 @@
+// const btn = document.querySelector("#meta");
+// btn.addEventListener("click", function(e){
+//     e.preventDefault();
+//     const peso = document.querySelector("#peso");
+// });
+// URL DA API DE DADOS DE USUARIOS
+URL = urlBaseApi() + "usuarios";
+//============================================================()=====================================
+calculaMetaAgua();
+
+function urlBaseApi(){
+    if(window.location.href.includes("vercel"))
+        return "https://nutrischedule.vercel.app/api/";
+    else
+        return "http://localhost:3000/";
+}
+
+// GET - PROCEDIMENTO PARA OBTER UM USUARIO
+function obterUsuarioLogadoId() {
+  let usuarioLogado = obterDadosUsuarioLogadoSessao();
+
+  if (usuarioLogado != null) {
+      console.log('usuarioLogado', usuarioLogado.id);
+    return usuarioLogado.id;
+  }
+}
+
+
+async function getDadosUser() {
+  let id_usuario = obterUsuarioLogadoId();
+  let params = `idUsuario=${id_usuario}`;
+  var response = await fetch(`${URL}?${params}`);
+  if (response.ok) {
+    let jsonData = await response.json();
+    console.log('jsonData',jsonData);
+    return jsonData;
+  } else {
+    throw new Error('Erro retorno db');
+  }
+}
+
+async function calculaMetaAgua() {
+  let dados_user = await getDadosUser();
+  let peso = dados_user[0].peso;
+  console.log(peso);
+
+  const metadeagua = (peso * 35);
+  //show in the page
+  document.getElementById('metagua').innerText = `${metadeagua} ml`;
+}
+
+
+
+
